19356439756d74cf376ff88aa191efca90af6d40
Liu Xia <lxconan@gmail.com>
2018-03-17 23:27:46 +0800

    [liuxia] Remove pre-parser since it doesn't make any sense.

--- src/Axe.Cli.Parser/ArgsParser.cs
+++ src/Axe.Cli.Parser/ArgsParser.cs
@@ -41,7 +41,7 @@ namespace Axe.Cli.Parser
             
             try
             {
-                return new ArgsPreParser(definition).Parse(args);
+                return ParseInternal(args);
             }
             catch (ArgParsingException error)
             {
@@ -51,6 +51,39 @@ namespace Axe.Cli.Parser
             {
                 return new ArgsParsingResult(new ArgsParsingError(string.Join(" ", args), ArgsParsingErrorCode.Unknown));
             }
+        }
+        
+        ArgsParsingResult ParseInternal(IList<string> args)
+        {
+            /*
+             * (start) --[EoA]--> (no default? error|stop)
+             * (start) --[command]--> (save command|continue with command)
+             * (start) --[option(kv) in default command]--> (save default command|waiting value with command)
+             * (start) --[option-groups(f) in default command]--> (save default command|yield flag|continue with command)
+             * (start) --[other]--> (save default command|yield free value|continue with command-free value)
+             * 
+             * (continue with command) --[option(kv) in command]--> (waiting value with command)
+             * (continue with command) --[option-groups(f) in command]--> (yield flag|continue with command)
+             * (continue with command) --[EoA]--> (stop)
+             * (continue with command) --[other]--> (yield free value|continue with command-free value)
+             * 
+             * (waiting value with command) --[EoA]--> (error|stop)
+             * (waiting value with command) --[other]--> (yield key value|continue with command)
+             * 
+             * (continue with command-free value) --[EoA]--> (stop)
+             * (continue with command-free value) --[other] --> (yield free value|continue with command-free value)
+             */
+
+            var builder = new PreParserResultBuilder();
+            IPreParsingState state = new StartState(definition, builder);
+            foreach (string arg in args)
+            {
+                state = state.MoveToNext(arg);
+                if (state == null) { return builder.Build(); }
+            }
+
+            state.MoveToNext(null);
+            return builder.Build();
         }
     }
 }
